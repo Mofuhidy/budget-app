b3bf11db1eb79f584b3ea5d8f2231ba3b447c2fe
moFuhidy <mo7ammed9290@gmail.com>
2023-10-13 13:16:42 +0300

    Setup the models and migration

---
+++ app/models/exchange_group.rb
@@ -0,0 +1,4 @@
+class ExchangeGroup < ApplicationRecord
+  belongs_to :exhange
+  belongs_to :group
+end
---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+# Include default devise modules. Others available are:
+# :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :exchanges, class_name: 'exchange', foreign_key: 'author_id', dependent: :destroy
+  has_many :groups, dependent: :destroy
+end
