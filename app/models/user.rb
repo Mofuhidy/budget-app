b28229234bf2ed96159dc9098b2d2487367b61e3
moFuhidy <mo7ammed9290@gmail.com>
2023-10-16 12:57:00 +0300

    Add models validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :exchanges, class_name: 'exchange', foreign_key: 'author_id', dependent: :destroy
   has_many :groups, dependent: :destroy
+
+  validates :name, presence: true
 end
